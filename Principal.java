ef3235f99916ebcd809ff4516f450f3036d9d376
samiksha <samikshachaudhari11@gmail.com>
2025-03-15 16:22:38 +0530

    develop

---
+++ Principal.java
@@ -0,0 +1,14 @@
+package package11;
+
+ 
+import package12.Teacher;
+
+public class Principal extends Teacher                
+{
+       public static void main(String[] args)
+       {
+    	   Teacher.sub1();
+    	   Teacher.sub3();
+    	   
+       }
+}
